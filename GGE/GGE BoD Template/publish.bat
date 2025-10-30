08443ede7edaf36a010aff5c49f7db868c86e7b0
erik <erik@pgcis.com>
2025-10-30 18:46:18 -0500

    vault backup: 2025-10-30 18:46:18

---
+++ GGE/GGE BoD Template/publish.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\eriks\Documents\Obsidian\Saga\GGE\GGE BoD Template"
+node index.js %*
---
+++ GGE/GGE BoD Template/publish.ps1
@@ -0,0 +1,2 @@
+Set-Location "C:\Users\eriks\Documents\Obsidian\Saga\GGE\GGE BoD Template"
+node index.js $args[0]
